ac0f1bf7f70dd7b842a3cb029434516feca67a9b
hpf <995085226@qq.com>
2018-05-03 15:53:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hpf/pomelo/app/service/DemoService.java
@@ -0,0 +1,18 @@
+package com.example.hpf.pomelo.app.service;
+
+import com.jess.arms.base.BaseService;
+
+/**
+ * ================================================
+ * 展示 {@link BaseService} 的用法
+ * <p>
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class DemoService extends BaseService {
+    @Override
+    public void init() {
+
+    }
+}
